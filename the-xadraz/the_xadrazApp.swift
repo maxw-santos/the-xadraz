ce8f99242ab08337a542837e2a220523e5e40b64
maxwel <maxf40589@gmail.com>
2022-11-04 14:01:06 -0300

    Initial Commit

---
+++ the-xadraz/the_xadrazApp.swift
@@ -0,0 +1,17 @@
+//
+//  the_xadrazApp.swift
+//  the-xadraz
+//
+//  Created by maxwel gomes dos santos on 04/11/22.
+//
+
+import SwiftUI
+
+@main
+struct the_xadrazApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
