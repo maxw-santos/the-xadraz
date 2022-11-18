20c9712ed27a751db8758bed7b8eb0454654c31d
Raul Braga <raulbragauchoarz@gmail.com>
2022-11-18 14:34:02 -0300

    "Dia 18/11 alterações iniciais" "Primeira tela adicionada"

---
+++ the-xadraz/FirstView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  the-xadraz
+//
+//  Created by maxwel gomes dos santos on 04/11/22.
+//
+
+import SwiftUI
+
+struct FirstView: View {
+    var body: some View {
+        VStack {
+          Text("")
+                .background( Image("LoadTela")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+
+                        )
+
+        }
+        .padding()
+
+    }
+
+}
+
+struct FirstView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+
+        FirstView()
+
+    }
+
+}
