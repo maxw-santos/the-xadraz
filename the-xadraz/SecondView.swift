33c53d4d4cf40eb2faee69ca50dfe43edf7e7879
Raul Braga <raulbragauchoarz@gmail.com>
2022-11-22 14:53:25 -0300

    Adicionando segunda tela do programa - 22/11

---
+++ the-xadraz/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  the-xadraz
+//
+//  Created by Raul Braga on 22/11/22.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(")
+            }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
