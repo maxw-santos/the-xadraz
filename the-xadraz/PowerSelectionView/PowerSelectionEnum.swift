e48dfa63bef85dad88484c7370f6b857414e2a2f
Raul Braga <raulbragauchoarz@gmail.com>
2022-11-25 15:07:37 -0300

    Adição do novo tipo "powers", usados posteriormente para a mecânica de solução de poderes. - data: 25/11/2022

---
+++ the-xadraz/PowerSelectionView/PowerSelectionEnum.swift
@@ -0,0 +1,13 @@
+//
+//  PowerSelectionEnum.swift
+//  the-xadraz
+//
+//  Created by Raul Braga on 25/11/22.
+//
+
+import Foundation
+
+
+enum powers {
+    case bombardeioDeGuerra, ataqueAoPosto, campoFechado, acordoDeMercenarios
+}
