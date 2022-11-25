6077c7d0005175e656df7b1296b8ca4ab3709bba
maxwel <maxf40589@gmail.com>
2022-11-25 13:57:16 -0300

    segunda tela visual quase ok 25- 11

--- the-xadraz/PrincipalViews/SecondView.swift
+++ the-xadraz/PrincipalViews/SecondView.swift
@@ -22,15 +22,26 @@ struct SecondView: View {
                     .edgesIgnoringSafeArea(.all)
                     .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
                 VStack {
+                    Image("tex1")
+                        .position(x : 150 , y : 90 )
                     SelectionPowerButton()
+                        .position(x : 200, y: 50)
+                    Image("tex2")
+                        .position(x : 150 , y : 35 )
+                    
                     SelectionPowerButton()
+                        .position(x : 200, y: 50 )
+                    
+                    
                     HStack {
                         NavigationLink(destination: thirdview()) {
                             Image("PlayIcon")
+                            
                         }
-                        
+                        .position(x : 320 ,  y : 45)
                     }
                 }
+                
             }
         }.ignoresSafeArea()
             .edgesIgnoringSafeArea(.all)
